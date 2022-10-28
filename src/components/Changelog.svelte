368b8086d2cda58b21acd1d3a0ac39e8f820341a
Tommy McMillan <tommymcmillan@Tommys-MacBook-Pro.local>
2022-10-28 18:49:33 +0200

    added pipelines, ready to push live

---
+++ src/components/Changelog.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let title;
+    export let data;
+</script>
+
+<h1 class="text-3xl my-4">{title}</h1>
+
+<div class="text-left inline-flex flex-col justify-center items-center gap-4">
+    {#each data as { date, items, version }}
+        <div class="bg-neutral-100 p-4 dark:bg-neutral-700 w-full">
+            <h2 class="text-lg font-bold mt-2">Version {version}</h2>
+            <p class="italic mb-2">{date}</p>
+            <ul class="list-disc ml-8">
+                {#each items as item}
+                    <li>{item}</li>
+                {/each}
+            </ul>
+        </div>
+    {/each}
+</div>
---
+++ src/js/emegration-changelog.js
@@ -0,0 +1,5 @@
+export const changelog = [{
+    version: "0.0.1",
+    date: 'October 28, 2022',
+    items: ['First prototype Print & Play and Rules published.']
+}];
