6db726186cbb92bbf6c9ee1093142c1ce68ac70b
Tommy McMillan <tommymcmillan@Tommys-MacBook-Pro.local>
2022-11-02 23:01:24 +0100

    added v0.0.2

--- src/js/emegration-changelog.js
+++ src/js/emegration-changelog.js
@@ -1,4 +1,8 @@
 export const changelog = [{
+    version: "0.0.2",
+    date: 'November 2, 2022',
+    items: ['Rules: Changed "Overview" to "Summary" section, and "Objective" to "Overview" section', 'Rules: Added more information to the Overview and Setup sections.', 'Rules: Clarified Winning The Game section, explaining winning competitively or cooperatively.']
+}, {
     version: "0.0.1",
     date: 'October 28, 2022',
     items: ['First prototype Print & Play and Rules published.']
