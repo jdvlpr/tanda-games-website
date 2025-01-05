0c088d898aa33276f36fba4407a3ec56e6c9056b
Thomas <jdvlpr@icloud.com>
2025-01-05 18:15:05 +0100

    added version 0.0.7

--- src/js/emegration-changelog.js
+++ src/js/emegration-changelog.js
@@ -1,44 +1,63 @@
-export const changelog = [{
+export const changelog = [
+  {
+    version: "0.0.7",
+    date: "January 5, 2025",
+    items: [
+      "Rules: Clarified the rules for various example scenarios.",
+      "Rules: Separated list of recommended Life Card packs for 5 and 6 players",
+    ],
+  },
+  {
     version: "0.0.6",
-    date: 'November 18, 2022',
+    date: "November 18, 2022",
     items: [
-        'Rules: Renamed Action from Re-buy to Procure',
-        'Rules: Added recommended Life Card packs for 5-6 players',
-    ]
-}, {
+      "Rules: Renamed Action from Re-buy to Procure",
+      "Rules: Added recommended Life Card packs for 5-6 players",
+    ],
+  },
+  {
     version: "0.0.5",
-    date: 'November 13, 2022',
+    date: "November 13, 2022",
     items: [
-        'Rules: Rearranged sections & simplified fonts',
-        'Rules: Fixed typos',
-        'Rules: Modified some page\'s layout',
-    ]
-}, {
+      "Rules: Rearranged sections & simplified fonts",
+      "Rules: Fixed typos",
+      "Rules: Modified some page's layout",
+    ],
+  },
+  {
     version: "0.0.4",
-    date: 'November 9, 2022',
+    date: "November 9, 2022",
     items: [
-        'Rules: Added more examples.',
-        'Rules: Rearranged some sections.',
-        'Rules: Renamed "Border Crossing" to "Entrance Lane".',
-        'Rules: Modified placement of Entrance Lane Requirement Tokens.',
-        'Cards: Changed Entrance Lanes\' design, added characters.',
-        'Cards: Modified Career Cards',
-    ]
-}, {
+      "Rules: Added more examples.",
+      "Rules: Rearranged some sections.",
+      'Rules: Renamed "Border Crossing" to "Entrance Lane".',
+      "Rules: Modified placement of Entrance Lane Requirement Tokens.",
+      "Cards: Changed Entrance Lanes' design, added characters.",
+      "Cards: Modified Career Cards",
+    ],
+  },
+  {
     version: "0.0.3",
-    date: 'November 4, 2022',
+    date: "November 4, 2022",
     items: [
-        'Rules: Added more information in the Overview section.',
-        'Rules: Added more detailed explanations of cards.',
-        'Rules: Added a Contents section.',
-        'Rules: Fixed some typos',
-    ]
-}, {
+      "Rules: Added more information in the Overview section.",
+      "Rules: Added more detailed explanations of cards.",
+      "Rules: Added a Contents section.",
+      "Rules: Fixed some typos",
+    ],
+  },
+  {
     version: "0.0.2",
-    date: 'November 2, 2022',
-    items: ['Rules: Changed "Overview" to "Summary" section, and "Objective" to "Overview" section', 'Rules: Added more information to the Overview and Setup sections.', 'Rules: Clarified Winning The Game section, explaining winning competitively or cooperatively.']
-}, {
+    date: "November 2, 2022",
+    items: [
+      'Rules: Changed "Overview" to "Summary" section, and "Objective" to "Overview" section',
+      "Rules: Added more information to the Overview and Setup sections.",
+      "Rules: Clarified Winning The Game section, explaining winning competitively or cooperatively.",
+    ],
+  },
+  {
     version: "0.0.1",
-    date: 'October 28, 2022',
-    items: ['First prototype Print & Play and Rules published.']
-}];
+    date: "October 28, 2022",
+    items: ["First prototype Print & Play and Rules published."],
+  },
+];
