09561d77cfed5274e9cef9a45f79f4af0b6d84d4
Thomas <jdvlpr@icloud.com>
2026-07-24 18:29:30 +0200

    fix: update version in package.json, refine Toast styles, and adjust timeout duration

--- src/components/Toast.svelte
+++ src/components/Toast.svelte
@@ -17,12 +17,12 @@
 <div
     in:fly={{ y: 20, duration: 300 }}
     out:fade={{ duration: 200 }}
-    class="flex items-center justify-between p-2 rounded-md shadow-lg min-w-[260px] w-full text-left text-black dark:text-white pointer-events-auto backdrop-blur-md border {typeClasses[item.type]}"
+    class="flex items-center justify-between p-1 rounded-md min-w-[260px] w-full text-left text-black dark:text-white pointer-events-auto backdrop-blur-md border {typeClasses[item.type]}"
     role="alert"
 >
-    <span class="text-sm">{item.message}</span>
+    <span class="text-xs md:text-sm">{item.message}</span>
     
-    <button 
+    <!-- <button 
         onclick={() => toast.remove(item.id)} 
         class="ml-2 opacity-50 hover:opacity-100 transition-opacity cursor-pointer"  
         aria-label="Close"
@@ -30,5 +30,5 @@
         <svg xmlns="[http://www.w3.org/2000/svg](http://www.w3.org/2000/svg)" class="size-5" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
-    </button>
+    </button> -->
 </div>
