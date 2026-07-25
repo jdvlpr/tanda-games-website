c5488fb9d181726568d8323e2eeb1601eb7c6688
Thomas <jdvlpr@icloud.com>
2026-07-25 15:40:24 +0200

    bump

--- src/components/emigration-emulator/EmigrationEmulator.svelte
+++ src/components/emigration-emulator/EmigrationEmulator.svelte
@@ -830,21 +830,8 @@ const isDev = import.meta.env.DEV;
   <div class="mb-4 flex flex-col items-center gap-2">
     <h1 class="text-center text-lg uppercase tracking-widest">Emigration Game Emulator</h1>
     <div class="flex gap-4 flex-wrap justify-center">
-      <a href="/emigration" class="btn-sm w-fit">← Exit</a>
-      <a href={`/files/emigration/v${VERSION}/emigration-game-rulebook-v${VERSION}.pdf`} target="_blank" download={`emigration-game-rulebook-v${VERSION}.pdf`} class="btn-sm w-fit"> <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="size-5"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                    />
-                </svg> Rulebook</a>
+      <a href="/emigration" class="underline w-fit">← Exit</a>
+      <a href={`/files/emigration/v${VERSION}/emigration-game-rulebook-v${VERSION}.pdf`} target="_blank" download={`emigration-game-rulebook-v${VERSION}.pdf`} class="underline flex items-center  gap-1 w-fit">  Rulebook (PDF)</a>
     </div>
   </div>
 
